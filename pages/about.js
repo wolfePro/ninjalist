b8bc156ab351cd9958bf1302263ce003d0db065e
Wolfe <thomas.wolfe@infinitive.com>
2021-09-09 11:38:04 -0400

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="Ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Random paragraph about nothing</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
